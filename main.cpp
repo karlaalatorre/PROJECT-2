7809a7aebe2958a8df2e6e28fd451c75fbef05fc
karlaalatorre <alatorrec.karlav@hotmail.com>
2015-07-24 00:16:17 -0700

    Signed-off-by: karlaalatorre <alatorrec.karlav@hotmail.com>

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
+
+int checkwin();
+void board();
+
+int main()
+{
+	int player = 1,i,choice;
+
+	char mark;
+	do
+	{
+		board();
+		player=(player%2)?1:2;
+
+		cout << "Player " << player << ", enter a number:  ";
+		cin >> choice;
+        }
+}
+		
